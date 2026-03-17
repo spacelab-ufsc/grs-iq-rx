f8c804339030194014a5532dcc9e676c65b17d4b
arthurmarr <arthurmarraui@gmail.com>
2026-03-17 01:40:48 -0300

    Parameters now passed through command line

--- python_application/iq_receiver.py
+++ python_application/iq_receiver.py
@@ -1,39 +1,45 @@
 import zmq
 import numpy as np
+import argparse
 from pluto import Pluto
 from usrp import USRP
 
 BUFFER_SIZE_BYTES = 2097152 # 2 MB
 
 class IQ_Receiver:
-    def __init__(self, config):
+    def __init__(self):
+        parser = argparse.ArgumentParser(description="GRS IQ Receiver")
+        parser.add_argument("-s", "--sdr", type=str, required=True, choices=["pluto", "usrp"])
+        parser.add_argument("-f", "--frequency", type=lambda x: int(float(x)), required=True)
+        parser.add_argument("-r", "--sample_rate", type=lambda x: int(float(x)), required=True)
+        parser.add_argument("-g", "--gain", type=float, required=True)
+        config = parser.parse_args()
+        
         self.context = zmq.Context()
 
         self.pub_socket = self.context.socket(zmq.PUB)
-        self.pub_socket.setsockopt(zmq.SNDHWM, 10)   # Max of 10 messages queued on sender (2 MB each)
-        self.pub_socket.setsockopt(zmq.RCVHWM, 10)   # Max of 10 messages queued in receiver
+        self.pub_socket.setsockopt(zmq.SNDHWM, 10)
+        self.pub_socket.setsockopt(zmq.RCVHWM, 10)
         self.pub_socket.bind("tcp://*:5556")
 
         self.cmd_socket = self.context.socket(zmq.SUB)
         self.cmd_socket.connect("tcp://localhost:5557")
         self.cmd_socket.setsockopt_string(zmq.SUBSCRIBE, "tune")
 
-        sdr_type = config["SDR"]
+        sdr_type = config.sdr
         if sdr_type.lower() == "pluto":
-            self.sdr = Pluto(sample_rate=config["sample_rate"], gain=config["gain"])
-            self.center_freq = config["freq"]
-            self.sdr.rx_lo = int(self.center_freq)
+            self.sdr = Pluto(sample_rate=config.sample_rate, gain=config.gain)
         elif sdr_type.lower() == "usrp":
-            self.sdr = USRP(sample_rate=config["sample_rate"], gain=config["gain"])
-            self.center_freq = config["freq"]
-            self.sdr.rx_lo = int(self.center_freq)
+            self.sdr = USRP(sample_rate=config.sample_rate, gain=config.gain)
         else:
-            raise ValueError(f"Invalid SDR chosen: {sdr_type}")
+            raise ValueError(f"Invalid SDR chosen: {sdr_type}\nMust be either usrp or pluto")
+        self.center_freq = config.frequency
+        self.sdr.rx_lo = self.center_freq
 
         self._iq_buffer = []
         self._buffered_bytes = 0
 
-    def _handle_commands(self):
+    def _handle_freq_synth(self):
         try:
             topic, new_freq_bytes = self.cmd_socket.recv_multipart(flags=zmq.NOBLOCK)
             new_freq = float(new_freq_bytes.decode())
@@ -45,9 +51,9 @@ class IQ_Receiver:
     def run(self):
         print("IQ Receiver running")
         while True:
-            self._handle_commands()
+            self._handle_freq_synth()
 
-            iq_samples = self.sdr.rx()  # Returns complex64 np array
+            iq_samples = self.sdr.rx()
             chunk = iq_samples.astype(np.complex64)
             chunk_bytes = chunk.tobytes()
 
@@ -58,11 +64,5 @@ class IQ_Receiver:
                 message = b"".join(self._iq_buffer)
                 self.pub_socket.send_multipart([b"iq_data", message])
 
-                # Reset buffer
                 self._iq_buffer = []
                 self._buffered_bytes = 0
-
-if __name__ == "__main__":
-    conf = {"sample_rate": 1e6, "gain": 30, "freq": 420.9e6, "SDR": "pluto"}
-    rx = IQ_Receiver(conf)
-    rx.run()
